df6f695125ad16263c7f2c0ab24093d501ff5aa2
6gb <63697672+6gb@users.noreply.github.com>
2021-09-06 14:32:23 -0300

    Dockerfiles update

---
+++ postgres/docker-entrypoint-initdb.d/postgres.sql
@@ -0,0 +1,49 @@
+create database farmacia_escola_licitacao;
+
+\c farmacia_escola_licitacao;
+
+create table Fornecedor (
+	id serial primary key,
+	cnpj char(12),
+	nome varchar(100) unique not null,
+	endereco varchar(200) unique,
+	telefone varchar(20) unique,
+	email varchar(50) unique,
+	sicaf varchar(50) unique
+);
+
+create table MateriaPrima (
+	id serial primary key,
+	descricao varchar(100) unique not null,
+	cas varchar(10) not null,
+	dcb varchar(100) not null,
+	dci varchar(100) not null
+);
+
+create table Embalagem (
+	id serial primary key,
+	descricao varchar(100) unique not null,
+	material varchar(50),
+	capacidade int
+);
+
+create table Licitacao (
+	id serial primary key,
+	dataCriacao date
+);
+
+create table FornecimentoMateriaPrima (
+	idLicitacao int,
+	idFornecedor int,
+	idMateriaPrima int,
+	preco numeric(6,2),
+	primary key (idLicitacao, idFornecedor, idMateriaPrima)
+);
+
+create table FornecimentoEmbalagem (
+    idLicitacao int,
+    idFornecedor int,
+    idEmbalagem int,
+    preco numeric(6,2),
+    primary key (idLicitacao, idFornecedor, idEmbalagem)
+);
